4eb99a43ecf340fddf38e2fc2add5811023a353b
VascoMiguens <v_miguens@hotmail.com>
2023-06-05 16:35:46 +0100

    - toggle mute/unmute - mute/unmute button - video loop

--- src/app/components/videobackground.tsx
+++ src/app/components/videobackground.tsx
@@ -1,11 +1,51 @@
 "use client"
 import YouTube from 'react-youtube';
-
+import {BiVolumeMute, BiVolumeFull} from 'react-icons/bi';
+import React, { useState, useEffect } from 'react';
 
 
 const VideoBackground = () => {
   //Video ID
   const videoId = 'GA4GE9EAPz0';
+
+  //State for mute/unmute
+  const [isMuted, setIsMuted] = useState(true);
+  const [player, setPlayer] = useState<any>(null);
+
+  //Toggle mute/unmute effect
+  useEffect(() => {
+    if (player) {
+      if (isMuted) {
+        player.mute();
+      } else {
+        player.unMute();
+      }
+    }
+  }, [player, isMuted]);
+
+  //Handler for when the YouTube player is ready
+  const handleOnReady = (event: any) => {
+    const youtubePlayer = event.target;
+    if (youtubePlayer) {
+      setPlayer(youtubePlayer);
+    }
+  };
+
+  //Handler for mute/unmute button click
+  const handleToggleMute = () => {
+    setIsMuted(!isMuted);
+  };
+
+  //Handler for when the video ends
+  const handleVideoEnd = () => {
+    if (player) {
+      //Seek to the start of the video
+      player.seekTo(0); 
+      //Play the video again
+      player.playVideo(); 
+    }
+  };
+
 
   //Video options
   const opts = {
@@ -19,13 +59,27 @@ const VideoBackground = () => {
       modestbranding: 1,
       fs: 0,
       mute: 1,
+      playsinline: 1,
+      iv_load_policy: 3,
+      start: 0,
+      end: 180,
+      autohide: 1,
+      disablekb: 1,
+      enablejsapi: 1,
+      origin: typeof window !== 'undefined' ? window.location.origin : '',
+      quality: 'hd1440',
     },
   };
 
   return (
     <>
       <div className="video-background">
-        <YouTube className='youtube-video z-1' videoId={videoId} opts={opts}/>
+        <YouTube className='youtube-video z-1' videoId={videoId} opts={opts} onReady={handleOnReady} onEnd={handleVideoEnd}/>
+        <div className="z-2 flex justify-end absolute bottom-5 w-10 right-5 text-4xl text-white">
+          <button onClick={handleToggleMute}>
+            {isMuted ? <BiVolumeMute/> : <BiVolumeFull/>}
+          </button>
+        </div>
       </div>
       
     </>
