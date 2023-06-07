296d0bea4174a8ee9961c1632122003931254189
VascoMiguens <v_miguens@hotmail.com>
2023-06-07 20:07:59 +0100

    - display current album tracks

--- src/app/components/music.tsx
+++ src/app/components/music.tsx
@@ -11,11 +11,20 @@ type Album = {
   images: Array<{ url: string }>;
 };
 
+// Track type
+type Track = {
+  id: string;
+  name: string;
+  preview_url: string;
+};
+
 const Music = () => {
     // State for albums
     const [albums, setAlbums] = useState<Album[]>([]);
     // State for current album index
     const [currentAlbumIndex, setCurrentAlbumIndex] = useState(0);
+    // State for current track index
+    const [tracks, setTracks] = useState<Track[]>([]);
     // Spotify service
     const spotifyService = SpotifyService.getInstance();
     
@@ -28,10 +37,17 @@ const Music = () => {
         const artistAlbums = await spotifyService.getArtistAlbums('3Hdsk1E0X4oe9nHDYJqQYY');
         // Set albums
         setAlbums(artistAlbums);
-        console.log(artistAlbums);
     };
     fetchData();
     }, []);
+
+    // Select album
+    const selectAlbum = async (albumId: string) => {
+        // Get album tracks
+        const albumTracks = await spotifyService.getAlbumTracks(albumId);
+        // Set tracks
+        setTracks(albumTracks);
+    };
 
     // Previous album
     const prevAlbum = () => {
@@ -48,13 +64,22 @@ const Music = () => {
         prevIndex === albums.length - 1 ? 0 : prevIndex + 1
         );
     };
-
+    
+    // Select current album
+    useEffect(() => {
+        // Get current album
+        const currentAlbum = albums[currentAlbumIndex];
+        // If current album exists, select it
+        if (currentAlbum) {
+            selectAlbum(currentAlbum.id);
+        }
+    }, [currentAlbumIndex, albums]);
 
     return (
     <div className="bg-black h-screen relative">
         <div className="bg-overlay h-screen">
             <h1 className="text-7xl text-gray-500 flex justify-end font-bold mr-5 p-5">Music</h1>
-            <div className="flex items-center justify-center">
+            <div className="flex justify-center">
                 <div className="ml-10 flex flex-col basis-1/2 mr-5">
                     <div className="flex text-white justify-between">
                         <button className="prev text-5xl" onClick={prevAlbum}>
@@ -75,8 +100,22 @@ const Music = () => {
                         <img src={albums[currentAlbumIndex]?.images[0]?.url} alt={albums[currentAlbumIndex]?.name}  className="mt-5" width={250}/>
                     </div>
                 </div>
-                <div className="ml-10 basis-1/2 flex justify-center mr-5">
-
+                <div className="ml-10 basis-1/3 flex justify-start items-start mt-10">
+                    <div className="track-list text-white">
+                        <p className="text-2xl mb-5">Song List</p>
+                        {tracks.length > 0 ? (
+                            tracks.map((track) => (
+                                <div
+                                key={track.id}
+                                className={"track-item"}
+                                >
+                                <p>{track.name}</p>
+                                </div>
+                            ))
+                        ) : (
+                            <p>No tracks available</p>
+                        )}
+                    </div>
                 </div>
             </div>
         </div>
